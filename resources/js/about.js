664ee8ce7f1d13e5e42c9774422c0bf234b23855
kenil <keniljoshi3@gmail.com>
2021-11-06 18:45:05 +0530

    about.js is changed

--- resources/js/about.js
+++ resources/js/about.js
@@ -1,6 +1,6 @@
 const profileClick = document.getElementById("userProfile");
 const fullProfileNavigator = document.querySelector(".fullProfileNavigator");
-
+console.log("kenil joshi");
 profileClick.addEventListener("click", () => {
   console.log("kenil");
   fullProfileNavigator.classList.toggle("profileNavigatorAnimator");
