13585d0622f97679bed4c71546242d8904f22cef
kenil <keniljoshi3@gmail.com>
2021-12-19 18:10:09 +0530

    make it better

--- app/mongoose/mongoose.js
+++ app/mongoose/mongoose.js
@@ -2,12 +2,10 @@ const mongoose = require("mongoose");
 
 const DB_URL =
   "mongodb+srv://Bkj:Bkj%405454721@cluster0.k3tur.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-mongoose.connect(
-  "mongodb+srv://Bkj:Bkj%405454721@cluster0.k3tur.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-  {
-    useNewUrlParser: true,
-  }
-);
+mongoose.connect(DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 //const connection=mongoose.Connection
 // connection.once('open',()=>{
 //     console.log('database connected...');
